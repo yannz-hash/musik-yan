ab2c1e803d3e925ea85ec3a2376711a8fa063d91
yannz-hash <chisato230902@gmail.com>
2025-12-16 23:52:04 +0700

    Initialize Discord bot with commands and events

---
+++ index.js
@@ -0,0 +1,58 @@
+const { Client, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+// Inisialisasi Client
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+client.queues = new Map();
+client.repeatModes = new Map();
+
+// Load Commands
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    client.commands.set(command.name, command);
+    
+    if (command.aliases) {
+        command.aliases.forEach(alias => {
+            client.aliases.set(alias, command.name);
+        });
+    }
+}
+
+// Load Events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+// Error Handling
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
+// Login Bot
+client.login(process.env.DISCORD_TOKEN);
